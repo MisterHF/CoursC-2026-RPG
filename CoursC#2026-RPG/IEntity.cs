591497cf2e4028279569c94e0e109e178f95d83b
MisterHF <h.florianpro@gmail.com>
2026-01-06 12:01:09 +0100

    Add entity interface

---
+++ CoursC#2026-RPG/IEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CoursC_2026_RPG
+{
+    public interface IEntity //, IStats
+    {
+        Entity CreateEntity();
+
+    }
+    public class Entity : IEntity
+    {
+        public Entity CreateEntity()
+        {
+            Console.WriteLine("Entity created");
+            return this;
+        }
+
+        public void SetStatsEntity()// Argument. (StateType statType, int value)
+        {
+            //Stats.Set(statType, value);
+            Console.WriteLine("Set values finished");
+        }
+    }
+}

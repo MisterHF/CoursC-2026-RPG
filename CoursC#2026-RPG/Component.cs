7449b7a975979db6175e39e81b14890d374a023f
mateo809 <150682933+mateo809@users.noreply.github.com>
2026-01-06 12:00:50 +0100

    Game

---
+++ CoursC#2026-RPG/Component.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CoursC_2026_RPG
+{
+    public interface IAttack
+    {
+        int ExecuteAttack(Weapon weapon);
+    }
+
+    public class NormalAttack : IAttack
+    {
+        public int ExecuteAttack(Weapon weapon)
+        {
+            Console.WriteLine("normale");
+            return weapon.GetDamage();
+        }
+    }
+
+    public class CriticalAttack : IAttack
+    {
+        public int ExecuteAttack(Weapon weapon)
+        {
+            Console.WriteLine("critique");
+            return weapon.GetDamage() * 2;
+        }
+    }
+
+    public abstract class Weapon
+    {
+        public abstract int GetDamage();
+    }
+
+    public abstract class WeaponDecorator : Weapon
+    {
+        protected Weapon weapon;
+
+        protected WeaponDecorator(Weapon weapon)
+        {
+            this.weapon = weapon;
+        }
+    }
+
+    public class FireDecorator : WeaponDecorator
+    {
+        public FireDecorator(Weapon weapon) : base(weapon) { }
+
+        public override int GetDamage()
+        {
+            Console.WriteLine("Feu");
+            return weapon.GetDamage() + 5;
+        }
+    }
+
+    public class PoisonDecorator : WeaponDecorator
+    {
+        public PoisonDecorator(Weapon weapon) : base(weapon) { }
+
+        public override int GetDamage()
+        {
+            Console.WriteLine("Poison");
+            return weapon.GetDamage() + 3;
+        }
+    }
+
+    public class Player
+    {
+        public IAttack Attack { get; set; }
+        public Weapon Weapon { get; set; }
+
+        public void DoAttack()
+        {
+            int damage = Attack.ExecuteAttack(Weapon);
+            Console.WriteLine($"Degat : {damage}");
+        }
+    }
+}
---
+++ CoursC#2026-RPG/Stats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CoursC_2026_RPG.Core
+{
+    public interface IStats
+    {
+        int Get(StatType stat);
+        void Set(StatType stat, int value);
+        void Add(StatType stat, int value);
+    }
+
+    public class Stats : IStats
+    {
+        private Dictionary<StatType, int> values = new Dictionary<StatType,int>();
+
+        public int Get(StatType stat)
+        {
+            return values.TryGetValue(stat, out int value) ? value : 0;
+        }
+
+        public void Set(StatType stat, int value)
+        {
+            values[stat] = value;
+        }
+
+        public void Add(StatType stat, int value)
+        {
+            Set(stat, Get(stat) + value);
+        }
+    }
+}
